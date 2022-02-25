68dd9f04376fd6f6058459eead96189cccb6c6d6
rohangupta788 <rohangupta788@gmail.com>
2022-02-25 20:18:17 +0530

    Third

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,61 @@
-import React, {Component, useEffect, useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../styles/App.css';
 
 const App = ({slides}) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [prevFlag, setPrevFlag] = useState(true);
+  const [nextFlag, setNextFlag] = useState(false);
+  const [restartFlag, setRestartFlag] = useState(true);
 
-  const [index,setIndex] = useState(0);
-  const [prev,setPrev] = useState("none")
-  const [next,setNext] = useState("inline")
-  const [restart,setRestart] = useState("none")
-
-  const data = slides[index];
-
-  useEffect(()=>{
-
-    if(index===slides.length -1){
-      setNext("none")
-    } 
-
-    else if(index!==0){
-      setPrev("inline")
-      setRestart("inline")
-      setNext("inline")
+  useEffect(() => {
+    // if current slide is the first one
+    if(slideIndex === 0) {
+      setPrevFlag(true);
+      setRestartFlag(true);
+    } else {
+      setPrevFlag(false);
+      setRestartFlag(false);
     }
-    else if(index===0){
-      setPrev("none")
-      setRestart("none")
-      setNext("inline")
-    }
-  
 
-  },[index])
+    // if current slide is the last one
+    if(slideIndex === slides.length - 1) {
+      setNextFlag(true);
+    } else {
+      setNextFlag(false);
+    }
+
+  }, [slideIndex])
+
+  const handlePrevClick = () => {
+      
+      setSlideIndex((prevState) => prevState - 1);
+  }
+
+  const handleNextClick = () => {
+      setSlideIndex((prevState) => prevState + 1);
+  }
+
+  const handleRestartClick = () => {
+    setSlideIndex(0);
+  }
+
 
 
   return (
-    <div>
-   <h1 data-testid="title">{data.title}</h1>
-   <p data-testid="text">{data.text}</p>
-
-   <button data-testid="button-restart" style={{display:restart}}
-  onClick={()=>{setIndex(0)}}>Restart</button>
-
-  <button data-testid="button-prev" style={{display:prev}}
-   onClick={()=>{setIndex(index-1)}}>Prev</button>
-
-  <button data-testid="button-next"
-   style={{display:next}}
-   onClick={()=>{setIndex(index+1)}}>Next</button>
-
- 
-
-    </div>
+    <>
+      <h1 id="heading">Amazon Prime: Trending</h1>
+      <div>
+        <div>
+          <h1 data-testid="title">{slides[slideIndex].title}</h1>
+          <p data-testid="text">{slides[slideIndex].text}</p>
+        </div>
+        
+        <button data-testid="button-prev" onClick={handlePrevClick} disabled={prevFlag}>Prev</button>
+        <button data-testid="button-next" onClick={handleNextClick} disabled={nextFlag}>Next</button>
+        <button data-testid="button-restart" onClick={handleRestartClick} disabled={restartFlag}>Restart</button>
+    
+      </div>
+    </>
   )
 }
 

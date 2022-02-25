979bbe6a283b10cadf2896273607078c43e519b3
rohangupta788 <rohangupta788@gmail.com>
2022-02-25 19:23:51 +0530

    Second

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,52 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import '../styles/App.css';
 
 const App = ({slides}) => {
 
   const [index,setIndex] = useState(0);
+  const [prev,setPrev] = useState("none")
+  const [next,setNext] = useState("inline")
+  const [restart,setRestart] = useState("none")
 
   const data = slides[index];
-  console.log(data);
+
+  useEffect(()=>{
+
+    if(index===slides.length -1){
+      setNext("none")
+    } 
+
+    else if(index!==0){
+      setPrev("inline")
+      setRestart("inline")
+      setNext("inline")
+    }
+    else if(index===0){
+      setPrev("none")
+      setRestart("none")
+      setNext("inline")
+    }
+  
+
+  },[index])
+
 
   return (
     <div>
    <h1 data-testid="title">{data.title}</h1>
    <p data-testid="text">{data.text}</p>
-   {index!==0 ?<button data-testid="button-prev"
-   onClick={()=>{setIndex(index-1)}}>Prev</button> :null}
 
-   {index!==slides.length-1?<button data-testid="button-next"
-   onClick={()=>{setIndex(index+1)}}>Next</button>: null}
+   <button data-testid="button-restart" style={{display:restart}}
+  onClick={()=>{setIndex(0)}}>Restart</button>
 
-{index!==0 ? <button data-testid="button-restart"
-onClick={()=>{setIndex(0)}}>Restart</button>:null}
+  <button data-testid="button-prev" style={{display:prev}}
+   onClick={()=>{setIndex(index-1)}}>Prev</button>
+
+  <button data-testid="button-next"
+   style={{display:next}}
+   onClick={()=>{setIndex(index+1)}}>Next</button>
+
+ 
 
     </div>
   )
